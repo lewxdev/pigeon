93f0513d080b917afcfdc4490396309af41aa25e
J. Lewis <inquiries.lewis+dev@gmail.com>
2021-02-01 09:25:10 -0500

    Updates

---
+++ precache-manifest.f5d7943df01f85d71210d21c0dbfaf3f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902597f896ad6318a9654ead6015c8d4",
+    "url": "/pigeon/index.html"
+  },
+  {
+    "revision": "dabf5940e2dbf7590730",
+    "url": "/pigeon/static/css/main.e20afbaf.chunk.css"
+  },
+  {
+    "revision": "45eb553fb4d8d55517b0",
+    "url": "/pigeon/static/js/2.f4ae5f14.chunk.js"
+  },
+  {
+    "revision": "dabf5940e2dbf7590730",
+    "url": "/pigeon/static/js/main.5625dfa0.chunk.js"
+  },
+  {
+    "revision": "91c396c53112636d2f97",
+    "url": "/pigeon/static/js/runtime-main.300e2173.js"
+  },
+  {
+    "revision": "cf1c0d66a99bd6e3b210091ea362c858",
+    "url": "/pigeon/static/media/delete.cf1c0d66.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/pigeon/static/media/heart.7746f5e4.svg"
+  },
+  {
+    "revision": "0f0a056e8d5201f34189fc50f0566f97",
+    "url": "/pigeon/static/media/heart_filled.0f0a056e.svg"
+  },
+  {
+    "revision": "6598f8347c78b327676fa4a20f479975",
+    "url": "/pigeon/static/media/home.6598f834.svg"
+  },
+  {
+    "revision": "1dfa883dbc6ee36fa284066006024d4c",
+    "url": "/pigeon/static/media/loading.1dfa883d.svg"
+  },
+  {
+    "revision": "1c50a8330c9b1a2d53b05b7ada671c40",
+    "url": "/pigeon/static/media/logout.1c50a833.svg"
+  },
+  {
+    "revision": "b1ecdaea2e458faa599937d04306ce87",
+    "url": "/pigeon/static/media/ori_bird.b1ecdaea.svg"
+  },
+  {
+    "revision": "5a6ee94f89043fe17577a955b5c00ad6",
+    "url": "/pigeon/static/media/settings.5a6ee94f.svg"
+  },
+  {
+    "revision": "a70d8f6db7b332ca98b05e4095fd57fb",
+    "url": "/pigeon/static/media/user.a70d8f6d.svg"
+  }
+]);
